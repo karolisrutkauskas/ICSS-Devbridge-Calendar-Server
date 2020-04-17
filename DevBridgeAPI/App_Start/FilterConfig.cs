f3ce78ef589670e38b77337388d8f39372577256
Julius Rupšys <julius.rupsys98@gmail.com>
2020-04-17 12:37:58 +0300

    Added application insights connected service, auto-generated code to integrate it; Updated Nu-Get packages, js files; Updated Web.{Publish|Debug}.config profiles to support Azure database connections strings on publishing; other minor changes

--- DevBridgeAPI/App_Start/FilterConfig.cs
+++ DevBridgeAPI/App_Start/FilterConfig.cs
@@ -9,7 +9,7 @@ namespace DevBridgeAPI
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
