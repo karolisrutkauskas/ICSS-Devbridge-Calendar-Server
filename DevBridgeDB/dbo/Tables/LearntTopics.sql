f498ca5fa366c51c8ed4ce9102beb93de5a287a4
Julius Rupšys <julius.rupsys98@gmail.com>
2020-05-16 18:05:32 +0300

    New database table: LearntTopics. Execute publish script on your databases

---
+++ DevBridgeDB/dbo/Tables/LearntTopics.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[LearntTopics]
+(
+	[TopicId] INT NOT NULL, 
+    [UserId] INT NOT NULL, 
+    CONSTRAINT PK_LearntTopics PRIMARY KEY (TopicId, UserId ASC),
+    CONSTRAINT FK_LearntTopics_Topics FOREIGN KEY (TopicId) REFERENCES Topics (TopicId) ON DELETE CASCADE,
+    CONSTRAINT FK_LearntTopics_Users FOREIGN KEY (UserId) REFERENCES Users (UserId) ON DELETE CASCADE
+)
